57cfbb716785e42ea47a1b051d8be0d3bd8d6a35
aeorxc <aeorxc@gmail.com>
2020-10-22 16:42:15 +0100

    release 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pylim",
-    version="1.1.0",
+    version="1.1.1",
     author="aeorxc",
     author_email="author@example.com",
     description="wrapper around morningstar commodity (LIM)",
