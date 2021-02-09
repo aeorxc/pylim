3f6a911aa728814a3b2a2e7bab2c2e32ed80d8db
Bartek Szostek <bartek.szostek@rwe.com>
2021-02-09 12:39:33 +0000

    chore: refactor to pytest, clean up/split tests

--- test/test_limuploader.py
+++ test/test_limuploader.py
@@ -1,29 +1,26 @@
 import pandas as pd
-from pylim import limuploader
+from datetime import date
+
+import pytest
+
 from pylim import lim
-import unittest
-from random import random
-from datetime import datetime
+from pylim import limuploader
 
 
-class TestLimUploader(unittest.TestCase):
-
-    def test_upload_series(self):
-        r1, r2 = round(random(), 2), round(random(), 2)
-        dn = datetime.now().date()
-        columns = ['TopRelation:Test:SPOTPRICE;TopColumn:Price:Close', 'TopRelation:Test:SPOTPRICE2']
-        data = {columns[0]: r1, columns[1]: r2}
-        df = pd.DataFrame(data, index=[dn])
-
-        dfmeta = {
-            'description': 'desc'
-        }
-
-        limuploader.upload_series(df, dfmeta)
-        df = lim.series(['SPOTPRICE', 'SPOTPRICE2'])
-        self.assertAlmostEqual(df.loc[dn.strftime('%Y-%m-%d')]['SPOTPRICE'], r1, 2)
-        self.assertAlmostEqual(df.loc[dn.strftime('%Y-%m-%d')]['SPOTPRICE2'], r2, 2)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_upload_series():
+    # Arrange.
+    spot_price1 = 1.21
+    spot_price2 = 4.89
+    today = date.today() 
+    data = {
+        'TopRelation:Test:SPOTPRICE1;TopColumn:Price:Close': spot_price1,
+        'TopRelation:Test:SPOTPRICE2': spot_price2,
+    }
+    upload_df = pd.DataFrame(data, index=[today])
+    dfmeta = {'description': 'desc'}
+    # Act.
+    limuploader.upload_series(upload_df, dfmeta)
+    download_df = lim.series(['SPOTPRICE1', 'SPOTPRICE2'])
+    # Assert
+    assert download_df.loc[f"{today:%Y-%m-%d}"]['SPOTPRICE1'] == pytest.approx(spot_price1)
+    assert download_df.loc[f"{today:%Y-%m-%d}"]['SPOTPRICE2'] == pytest.approx(spot_price2)
--- test/test_limutils.py
+++ test/test_limutils.py
@@ -1,15 +1,16 @@
+import pytest
+
 from pylim import limutils
-import unittest
 
 
-class TestLim(unittest.TestCase):
-
-    def test_pra_symbol(self):
-        self.assertFalse(limutils.check_pra_symbol('FB'))
-        self.assertTrue(limutils.check_pra_symbol('AAGXJ00'))
-        self.assertTrue(limutils.check_pra_symbol('PGACR00'))
-        self.assertTrue(limutils.check_pra_symbol('PA0005643.6.0'))
-
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize(
+    "symbol, result",
+    [
+        ("FB", False),
+        ("AAGXJ00", True),
+        ("PGACR00", True),
+        ("PA0005643.6.0", True),
+    ]
+)
+def test_pra_symbol(symbol: str, result: bool):
+    assert limutils.check_pra_symbol(symbol) == result
