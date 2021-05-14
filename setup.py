e1fe4704302250e0d2c3b223951f9e899c3951e8
aeorxc <aeorxc@gmail.com>
2021-05-14 09:31:01 +0100

    fix: revert test change was accidently committed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pylim",
-    version="1.4.5",
+    version="1.4.6",
     author="aeorxc",
     description="Wrapper around morningstar commodity (LIM)",
     url="https://github.com/aeorxc/pylim",
